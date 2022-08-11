e92828ceae2830ae056c81ede345cfc5f70868b2
heguicun <heguicun7878@foxmail.com>
2022-08-11 11:24:40 +0800

    "名称更新"

--- app/src/main/java/com/dream/bjst/main/menu/TabManager.kt
+++ app/src/main/java/com/dream/bjst/main/menu/TabManager.kt
@@ -15,22 +15,43 @@ const val MAIN_TAB_MINE = 3
 const val KEY_TAB_POSITION = "position"
 
 object TabManager {
-    val menus:List<TabItem>
-    get() {
-       return arrayListOf<TabItem>().apply {
-           add(TabItem(MAIN_TAB_AREA, R.id.tab_lottie_1, R.string.title_area, "lottie_home.json"))
-           add(
-               TabItem(
-                   MAIN_TAB_SQUARE,
-                   R.id.tab_lottie_2,
-                   R.string.title_square,
-                   "lottie_category.json"
-               )
-           )
-           add(TabItem(MAIN_TAB_MESSAGE, R.id.tab_lottie_3, R.string.title_message, "lottie_rank.json"))
-           add(TabItem(MAIN_TAB_MINE, R.id.tab_lottie_5, R.string.title_mine, "lottie_mine.json"))
-       }
-    }
+    val menus: List<TabItem>
+        get() {
+            return arrayListOf<TabItem>().apply {
+                add(
+                    TabItem(
+                        MAIN_TAB_AREA,
+                        R.id.tab_lottie_1,
+                        R.string.title_area,
+                        "lottie_home.json"
+                    )
+                )
+                add(
+                    TabItem(
+                        MAIN_TAB_SQUARE,
+                        R.id.tab_lottie_2,
+                        R.string.title_square,
+                        "lottie_category.json"
+                    )
+                )
+                add(
+                    TabItem(
+                        MAIN_TAB_MESSAGE,
+                        R.id.tab_lottie_3,
+                        R.string.title_message,
+                        "lottie_rank.json"
+                    )
+                )
+                add(
+                    TabItem(
+                        MAIN_TAB_MINE,
+                        R.id.tab_lottie_5,
+                        R.string.title_mine,
+                        "lottie_mine.json"
+                    )
+                )
+            }
+        }
 }
 
 @IntDef(MAIN_TAB_AREA, MAIN_TAB_SQUARE, MAIN_TAB_MESSAGE, MAIN_TAB_MINE)

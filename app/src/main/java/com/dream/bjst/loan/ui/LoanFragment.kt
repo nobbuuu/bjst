840524da6bd2ff183ca3c439ec643913b201b9e0
tiaozi <1184824553@qq.com>
2022-08-14 17:05:08 +0800

    【FEAT】首页静态页面实现

---
+++ app/src/main/java/com/dream/bjst/loan/ui/LoanFragment.kt
@@ -0,0 +1,14 @@
+package com.dream.bjst.loan.ui
+
+import android.os.Bundle
+import com.dream.bjst.databinding.FragmentLoanBinding
+import com.tcl.base.common.BaseViewModel
+import com.tcl.base.common.ui.BaseFragment
+
+class LoanFragment : BaseFragment<BaseViewModel, FragmentLoanBinding>() {
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+}

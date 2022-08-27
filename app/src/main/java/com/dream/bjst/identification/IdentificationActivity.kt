858b0165eb7a45c2fba98b2d67adfa35c4255259
Administrator <986812333@qq.com>
2022-08-27 14:35:29 +0800

    "认证界面"

--- app/src/main/java/com/dream/bjst/identification/IdentificationActivity.kt
+++ app/src/main/java/com/dream/bjst/identification/IdentificationActivity.kt
@@ -2,100 +2,33 @@ package com.dream.bjst.identification
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dream.bjst.R
+import com.dream.bjst.bean.EmptyBean
 import com.dream.bjst.databinding.ActivityIdentificationBinding
+import com.dream.bjst.identification.adapter.IdentifyAdapter
 import com.dream.bjst.identification.vm.IdentificationViewModel
 import com.tcl.base.common.ui.BaseActivity
 import com.tcl.base.kt.ktClick
 
 class IdentificationActivity :
     BaseActivity<IdentificationViewModel, ActivityIdentificationBinding>() {
-    var temp:Boolean = false
-    @SuppressLint("ResourceAsColor")
+     var identifyAdapter=IdentifyAdapter()
+     var mNameList=ArrayList<EmptyBean>()
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.identifyTitle.ktClick {
             onBackPressed()
         }
-        //Authentication
-        mBinding.authenticationRv.ktClick {
-            when(temp){
-                false -> {
-                    //设置Authentication
-                    mBinding.authenticationRv.setBackgroundResource(R.color.color_identify_select)
-                    mBinding.identifyAuthenticationArrow.setImageResource(R.mipmap.itentify_select_arrow)
-                    //
-                    mBinding.facialRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.facialAuthenticationArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    //
-                    mBinding.emergencyContactRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.emergencyContactArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    //
-                    mBinding.bankCountRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.bankCountArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    temp=true
-                }
-                true -> {
-                    mBinding.authenticationRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.identifyAuthenticationArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    temp=false
-                }
-            }
 
-        }
-        //Facial verification
-        mBinding.facialRv.ktClick {
-
-            when(temp){
-               false -> {
-                   //设置Facial verification
-                   mBinding.facialRv.setBackgroundResource(R.color.color_identify_select)
-                   mBinding.facialAuthenticationArrow.setImageResource(R.mipmap.itentify_select_arrow)
-                   temp=true
-               }
-                true -> {
-                    mBinding.facialRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.facialAuthenticationArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    temp=false
-                }
-            }
-
-        }
-        //Emergency contact
-        mBinding.emergencyContactRv.ktClick {
-            when(temp){
-                //Emergency contact
-                false -> {
-                    mBinding.emergencyContactRv.setBackgroundResource(R.color.color_identify_select)
-                    mBinding.emergencyContactArrow.setImageResource(R.mipmap.itentify_select_arrow)
-                    temp=true
-                }
-                true -> {
-                    mBinding.emergencyContactRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.emergencyContactArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    temp=false
-                }
-            }
-        }
-        //bankAccount
-        mBinding.bankCountRv.ktClick {
-            when(temp){
-
-                false -> {
-                    mBinding.bankCountRv.setBackgroundResource(R.color.color_identify_select)
-                    mBinding.bankCountArrow.setImageResource(R.mipmap.itentify_select_arrow)
-                    temp=true
-                }
-                true->{
-                    mBinding.bankCountRv.setBackgroundResource(R.color.color_identify_unselect)
-                    mBinding.bankCountArrow.setImageResource(R.mipmap.itentify_unselect_arrow)
-                    temp=false
-                }
-            }
-        }
     }
 
     override fun initData() {
-
+        mBinding.identifyRecyclerview.layoutManager=GridLayoutManager(this,2)
+       repeat(4){
+         mNameList.add(EmptyBean())
+     }
+        identifyAdapter.setList(mNameList)
     }
 
     override fun initDataOnResume() {

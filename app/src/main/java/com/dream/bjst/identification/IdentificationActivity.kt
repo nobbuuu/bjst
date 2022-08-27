d90def1ec7fe3732924ef5d3247a1c0ee234cd93
Administrator <986812333@qq.com>
2022-08-27 16:40:15 +0800

    "认证界面"

--- app/src/main/java/com/dream/bjst/identification/IdentificationActivity.kt
+++ app/src/main/java/com/dream/bjst/identification/IdentificationActivity.kt
@@ -1,34 +1,43 @@
 package com.dream.bjst.identification
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.dream.bjst.R
-import com.dream.bjst.bean.EmptyBean
+
 import com.dream.bjst.databinding.ActivityIdentificationBinding
 import com.dream.bjst.identification.adapter.IdentifyAdapter
+import com.dream.bjst.identification.bean.identifyBean
 import com.dream.bjst.identification.vm.IdentificationViewModel
 import com.tcl.base.common.ui.BaseActivity
 import com.tcl.base.kt.ktClick
+import com.tcl.base.kt.ktToastShow
 
 class IdentificationActivity :
     BaseActivity<IdentificationViewModel, ActivityIdentificationBinding>() {
+    val mNameList:MutableList<identifyBean> = ArrayList()
+
      var identifyAdapter=IdentifyAdapter()
-     var mNameList=ArrayList<EmptyBean>()
+
     override fun initView(savedInstanceState: Bundle?) {
         mBinding.identifyTitle.ktClick {
             onBackPressed()
         }
-
     }
 
     override fun initData() {
         mBinding.identifyRecyclerview.layoutManager=GridLayoutManager(this,2)
        repeat(4){
-         mNameList.add(EmptyBean())
+         mNameList.add(identifyBean(R.mipmap.identify_bank,"Authentication"))
      }
         identifyAdapter.setList(mNameList)
+        mBinding.identifyRecyclerview.adapter=identifyAdapter
+        //点击事件
+        identifyAdapter.setOnItemClickListener { adapter, view, position ->
+        "${position}".ktToastShow()
+
+        }
+
+
     }
 
     override fun initDataOnResume() {

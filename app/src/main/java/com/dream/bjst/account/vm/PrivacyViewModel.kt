8069044aa54f2a1da9e957f77e8bba1c195539ab
heguicun <heguicun7878@foxmail.com>
2022-09-05 00:28:11 +0800

    “account界面更改”

---
+++ app/src/main/java/com/dream/bjst/account/vm/PrivacyViewModel.kt
@@ -0,0 +1,11 @@
+package com.dream.bjst.account.vm
+
+import com.tcl.base.common.BaseViewModel
+
+/**
+ * 创建日期：2022-09-05 on 0:17
+ * 描述:衣带渐宽终不悔、为伊消得人憔悴
+ * 作者:HeGuiCun Administrator
+ */
+class PrivacyViewModel:BaseViewModel() {
+}

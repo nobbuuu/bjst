9331912a8fcedb083c5f218057b45db685bcf3e7
heguicun <heguicun7878@foxmail.com>
2022-09-05 13:53:42 +0800

    “account界面调试接口”

--- app/src/main/java/com/dream/bjst/account/ui/AccountDeleteActivity.kt
+++ app/src/main/java/com/dream/bjst/account/ui/AccountDeleteActivity.kt
@@ -1,7 +1,12 @@
 package com.dream.bjst.account.ui
 
 import android.os.Bundle
+import com.blankj.utilcode.util.GsonUtils
+import com.didichuxing.doraemonkit.util.LogUtils
+import com.dream.bjst.BuildConfig
+import com.dream.bjst.account.bean.AccountDeleteParam
 import com.dream.bjst.account.vm.AccountViewModel
+import com.dream.bjst.common.UserManager
 import com.dream.bjst.databinding.ActivityAccountDeleteBinding
 import com.tcl.base.common.ui.BaseActivity
 import com.tcl.base.kt.ktClick
@@ -21,6 +26,18 @@ class AccountDeleteActivity :BaseActivity<AccountViewModel,ActivityAccountDelete
     }
 
     override fun initData() {
+
+        var param=GsonUtils.toJson(
+            AccountDeleteParam(
+                `958484A29186879D9B9A` = BuildConfig.VERSION_NAME,
+                `978187809B999186BD90` = UserManager.getUserNo()as Int,
+                `9995869F9180BD90` =10022
+            )
+        )
+        LogUtils.dTag("paramJson",param)
+        viewModel.accountDeleteData(param)
+
+
         Thread {
             try {
                 Thread.sleep(1000)

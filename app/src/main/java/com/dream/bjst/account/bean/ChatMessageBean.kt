6b4232d4665e57290cb871420d52cde918f5b8f1
Administrator <986812333@qq.com>
2022-09-07 21:03:55 +0800

    “账户界面调整”

--- app/src/main/java/com/dream/bjst/account/bean/ChatMessageBean.kt
+++ app/src/main/java/com/dream/bjst/account/bean/ChatMessageBean.kt
@@ -6,8 +6,6 @@ package com.dream.bjst.account.bean
  * 作者:HeGuiCun Administrator
  */
 data class ChatMessageBean(
-
-
     val `978187809B999186B7958691B79C9580A495809C`: String, //客服聊天地址，获取到地址后拼接这些参数：customerId=【值】&userName=【值】&userPhone=【值】&customerUId=【值】。在webview里面打开url
     val `978187809B999186B7958691A49C9B9A91`: String, //客服电话
     val `978187809B999186A79186829D9791A7839D80979C`: Boolean, //自研客服系统是否开启 true开启 false关闭

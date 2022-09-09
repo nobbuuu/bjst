950643414ae95b6766e753a4670afa2b1009a4bb
Administrator <986812333@qq.com>
2022-09-09 21:59:18 +0800

    “还款界面调整”

--- app/src/main/java/com/dream/bjst/repayment/bean/RepaymentBean.kt
+++ app/src/main/java/com/dream/bjst/repayment/bean/RepaymentBean.kt
@@ -3,38 +3,38 @@ package com.dream.bjst.repayment.bean
 import java.io.Serializable
 
 data class RepaymentBean(
-    val dueTodayOrders: List<DueTodayOrder>,
-    val notDueOrders: List<NotDueOrder>,
-    val overdueOrders: List<OverdueOrder>
+    val `908191A09B90958DBB8690918687`: List<DueTodayOrder>,//当日到期订单
+    val `9A9B80B08191BB8690918687`: List<NotDueOrder>,//未到期订单
+    val `9B829186908191BB8690918687`: List<OverdueOrder>//逾期中的订单
 ):Serializable
 
 
 data class DueTodayOrder(
-    val borrowId: Int,
-    val dueDay: Int,
-    val icoUrl: String,
-    val overdueInterestRate: Int,
-    val productId: Int,
-    val productName: String,
-    val remainTotalAmount: Int
+    val `969B86869B83BD90`: Int,//借款编号
+    val `908191B0958D`: Int,//逾期订单为逾期天数,未到期订单为到期天数
+    val `9D979BA18698`: String,//图标url
+    val `9B829186908191BD9A809186918780A6958091`: Int,//逾期时的利率（即计算滞纳金的利率）
+    val `84869B90819780BD90`: Int,//产品id
+    val `84869B90819780BA959991`: String,//产品名称
+    val `869199959D9AA09B809598B5999B819A80`: Int//剩余总待还金额
 )
 
 data class NotDueOrder(
-    val borrowId: Int,
-    val dueDay: Int,
-    val icoUrl: String,
-    val overdueInterestRate: Int,
-    val productId: Int,
-    val productName: String,
-    val remainTotalAmount: Int
+    val `969B86869B83BD90`: Int,//借款编号
+    val `908191B0958D`: Int,//逾期订单为逾期天数,未到期订单为到期天数
+    val `9D979BA18698`: String,//图标url
+    val `9B829186908191BD9A809186918780A6958091`: Int,//逾期时的利率（即计算滞纳金的利率）
+    val `84869B90819780BD90`: Int,//产品id
+    val `84869B90819780BA959991`: String,//产品名称
+    val `869199959D9AA09B809598B5999B819A80`: Int//剩余总待还金额
 )
 
 data class OverdueOrder(
-    val borrowId: Int,
-    val dueDay: Int,
-    val icoUrl: String,
-    val overdueInterestRate: Int,
-    val productId: Int,
-    val productName: String,
-    val remainTotalAmount: Int
+    val `969B86869B83BD90`: Int,//借款编号
+    val `908191B0958D`: Int,//逾期订单为逾期天数,未到期订单为到期天数
+    val `9D979BA18698`: String,//图标url
+    val `9B829186908191BD9A809186918780A6958091`: Int,//逾期时的利率（即计算滞纳金的利率）
+    val `84869B90819780BD90`: Int,//产品id
+    val `84869B90819780BA959991`: String,//产品名称
+    val `869199959D9AA09B809598B5999B819A80`: Int//剩余总待还金额
 )

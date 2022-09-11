54ee99d549fcb60b95eabb789c6161230250ad10
heguicun <heguicun7878@foxmail.com>
2022-09-11 11:45:07 +0800

    “repayment界面实现”

---
+++ app/src/main/java/com/dream/bjst/repayment/adapter/OverDueAdapter.kt
@@ -0,0 +1,21 @@
+package com.dream.bjst.repayment.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.dream.bjst.R
+import com.dream.bjst.repayment.bean.OverdueOrder
+import com.dream.bjst.repayment.bean.RepaymentBean
+
+/**
+ * 创建日期：2022-09-11 on 9:41
+ * 描述:衣带渐宽终不悔、为伊消得人憔悴
+ * 作者:HeGuiCun Administrator
+ */
+class OverDueAdapter:BaseQuickAdapter<OverdueOrder, BaseViewHolder>(R.layout.item_repayment_in) {
+
+    override fun convert(holder: BaseViewHolder, item: OverdueOrder) {
+
+    }
+
+
+}
--- app/src/main/java/com/dream/bjst/repayment/ui/RepaymentFragment.kt
+++ app/src/main/java/com/dream/bjst/repayment/ui/RepaymentFragment.kt
@@ -1,6 +1,5 @@
 package com.dream.bjst.repayment.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,14 +7,12 @@ import android.view.View
 import android.widget.Button
 import androidx.navigation.Navigation
 import com.blankj.utilcode.util.BarUtils
-import com.blankj.utilcode.util.ToastUtils
 import com.dream.bjst.R
 
 import com.dream.bjst.databinding.FragmentRepaymentBinding
-import com.dream.bjst.repayment.adapter.RePaymentAdapter
-import com.dream.bjst.repayment.adapter.RePaymentInAdapter
+import com.dream.bjst.repayment.adapter.*
+import com.dream.bjst.repayment.bean.OverdueOrder
 import com.dream.bjst.repayment.bean.RepaymentBean
-import com.dream.bjst.repayment.bean.RepaymentInDetailBean
 import com.dream.bjst.repayment.vm.RepaymentViewModel
 import com.liveness.dflivenesslibrary.view.TimeViewContoller.TAG
 import com.tcl.base.common.ui.BaseFragment
@@ -27,14 +24,16 @@ import com.tcl.base.utils.MmkvUtil
  * 描述:衣带渐宽终不悔、为伊消得人憔悴
  * 作者:HeGuiCun Administrator
  */
-class RepaymentFragment:BaseFragment<RepaymentViewModel,FragmentRepaymentBinding>() {
-    var mRePaymentAdapter: RePaymentAdapter? = null
-    var mRePaymentInAdapter: RePaymentInAdapter? = null
-    var mList: MutableList<RepaymentBean>? = null
+class RepaymentFragment : BaseFragment<RepaymentViewModel, FragmentRepaymentBinding>() {
+    //数据适配器
+    var overDueAdapter = OverDueAdapter()
+    var dueTodayAdapter = DueTodayAdapter()
+    var notDueAdapter = NotDueAdapter()
 
     override fun initView(savedInstanceState: Bundle?) {
         BarUtils.addMarginTopEqualStatusBarHeight(mBinding.repayment)
         viewModel.repaymentData()
+        initRv()
     }
 
     /**
@@ -42,50 +41,32 @@ class RepaymentFragment:BaseFragment<RepaymentViewModel,FragmentRepaymentBinding
      */
     override fun startObserve() {
         super.startObserve()
-        viewModel.repaymentResult.observe(this){
-            Log.i(TAG, "startObserve: "+it.`908191A09B90958DBB8690918687`.size)
+        viewModel.repaymentResult.observe(this) {
+            overDueAdapter.setList(it.`9B829186908191BB8690918687`)
+            dueTodayAdapter.setList(it.`908191A09B90958DBB8690918687`)
+            notDueAdapter.setList(it.`9A9B80B08191BB8690918687`)
 
-
-
+            if (overDueAdapter.data.isEmpty() && dueTodayAdapter.data.isEmpty() && notDueAdapter.data.isEmpty()) {
+                // 显示空布局
+                getEmptyView()?.let {
+                    overDueAdapter.setEmptyView(it)
+                }
+            }
         }
     }
 
-    override fun initDataOnViewCreated() {
-        super.initDataOnViewCreated()
-        mRePaymentAdapter = RePaymentAdapter()
-        mRePaymentInAdapter = RePaymentInAdapter()
 
-        // 显示空布局
-        getEmptyView()?.let { mRePaymentAdapter?.setEmptyView(it) }
-        mBinding.repaymentRvPlan.adapter=mRePaymentAdapter
-        setData()
-    }
-    private fun setData() {
-        mList = ArrayList()
-        val inList: MutableList<RepaymentInDetailBean> = ArrayList()
-        for (i in 0..2) {
-            inList.add(RepaymentInDetailBean(11,"",22,33,44,"",55,66,""))
-        }
-        for (i in 0..9) {
-//            (mList as ArrayList<RepaymentBean>).add(RepaymentBean(inList))
-        }
-        mRePaymentAdapter?.setList(mList)
-        mRePaymentInAdapter?.setList(inList)
-
+    private fun initRv() {
+        mBinding.overDueRv.adapter = overDueAdapter
+        mBinding.dueTodayRv.adapter = dueTodayAdapter
+        mBinding.notDueRv.adapter = notDueAdapter
         //点击时间
         event()
     }
+
     private fun event() {
-        mRePaymentInAdapter?.setOnItemChildClickListener { adapter, view, position ->
-            startActivity(
-                Intent(activity, RepaymentDetailActivity::class.java)
-            )
-        }
-        mRePaymentAdapter?.setOnItemClickListener { adapter, view, position ->
-            startActivity(
-                Intent(activity, RepaymentDetailActivity::class.java)
-            )
-        }
+
+
     }
 
     /**
@@ -93,7 +74,8 @@ class RepaymentFragment:BaseFragment<RepaymentViewModel,FragmentRepaymentBinding
      */
     private fun getEmptyView(): View? {
         val view: View =
-            LayoutInflater.from(requireContext()).inflate(R.layout.item_repayment_empty, null, false)
+            LayoutInflater.from(requireContext())
+                .inflate(R.layout.item_repayment_empty, null, false)
         val btnLoadData = view.findViewById<Button>(R.id.btn_load_data)
         btnLoadData.setOnClickListener { v ->
             Navigation.findNavController(v)

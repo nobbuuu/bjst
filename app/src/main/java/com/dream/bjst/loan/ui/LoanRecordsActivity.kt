ed815c7e6b88e9265d1309fd7455b6452a906f57
heguicun <heguicun7878@foxmail.com>
2022-09-22 01:43:51 +0800

    “界面bug细节调整”

--- app/src/main/java/com/dream/bjst/loan/ui/LoanRecordsActivity.kt
+++ app/src/main/java/com/dream/bjst/loan/ui/LoanRecordsActivity.kt
@@ -1,6 +1,7 @@
 package com.dream.bjst.loan.ui
 
 import android.os.Bundle
+import androidx.navigation.Navigation
 import com.blankj.utilcode.util.GsonUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.dream.bjst.R
@@ -11,6 +12,7 @@ import com.dream.bjst.loan.adapter.LoanRecordsChildAdapter
 import com.dream.bjst.loan.bean.HistoryBean
 import com.dream.bjst.loan.bean.OrderResultBean
 import com.dream.bjst.loan.vm.LoanViewModel
+import com.dream.bjst.repayment.ui.RepaymentDetailActivity
 import com.dream.bjst.repayment.ui.RepaymentFragment
 import com.dream.bjst.utils.DataUtils
 import com.tcl.base.common.ui.BaseActivity
@@ -38,7 +40,7 @@ class LoanRecordsActivity : BaseActivity<LoanViewModel, ActivityLoanRecordsBindi
     override fun initData() {
         refreshData()
         mLoanRecordsChildAdapter.setOnItemChildClickListener { adapter, view, position ->
-            if (view.id == R.id.repayNow){
+            if (view.id == R.id.repayNow) {
                 setResult(921)
                 finish()
             }
@@ -54,24 +56,30 @@ class LoanRecordsActivity : BaseActivity<LoanViewModel, ActivityLoanRecordsBindi
         viewModel.fetchOrderHistory(GsonUtils.toJson(CommonPageBean(curPage, 20)))
     }
 
-    override fun initDataOnResume() {
-
-    }
-
     override fun startObserve() {
         super.startObserve()
         viewModel.historyData.observe(this) {
             mBinding.smartRefresh.finishRefresh()
             mBinding.smartRefresh.finishLoadMore()
             val list = arrayListOf<OrderResultBean>()
-            DataUtils.groupByMode("958484988DA09D9991", it.`9B86909186B89D8780`).forEach { data ->
-                list.add(OrderResultBean(applyTime = data.key, `9B86909186B89D8780` = data.value))
-            }
+            DataUtils.groupByMode("958484988DA09D9991", it.`9B86909186B89D8780`)
+                .forEach { data ->
+                    list.add(
+                        OrderResultBean(
+                            applyTime = data.key,
+                            `9B86909186B89D8780` = data.value
+                        )
+                    )
+                }
             if (curPage == 1) {
                 dataList.clear()
             }
             dataList.addAll(list)
             mLoanRecordsAdapter.setList(dataList)
         }
+    }
+    override fun initDataOnResume() {
+
+
     }
 }
--- app/src/main/java/com/dream/bjst/repayment/bean/ExtendRePaymentBean.kt
+++ app/src/main/java/com/dream/bjst/repayment/bean/ExtendRePaymentBean.kt
@@ -5,16 +5,16 @@ import java.io.Serializable
 
 data class ExtendRePaymentBean(
     val `969B86869B83BD90`: String,//订单编号
-    val `84869D9A979D849598B5999B819A80`: String,//借款金额
+    val `84869D9A979D849598B5999B819A80`: Int,//借款金额
     val `869184958DB19A90`: String,//到期日期（超过此时间为逾期）
     val `869B9898A691A4958DBD9A929BA0918699B89D8780`: List<RollRePayInfoTerm>//展期费用信息
 ):Serializable,BaseParamBean()
 
 
 data class RollRePayInfoTerm(
-    val `909198958DB5999B819A80`: String,//延期还款费用(延期的手续费)
+    val `909198958DB5999B819A80`: Int,//延期还款费用(延期的手续费)
     val `909198958DB0958D87`: String,//延期天数
     val `909198958DB19A90`: String,//展期/延期到期日期（超过此时间为逾期）
-    val `9B829186908191B5999B819A80`: String,//逾期费用
-    val `84959D90B5999B819A80`: String//应付金额(延期还款费用 + 逾期费用)
+    val `9B829186908191B5999B819A80`: Int,//逾期费用
+    val `84959D90B5999B819A80`: Int//应付金额(延期还款费用 + 逾期费用)
 )

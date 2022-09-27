f7be5c975b1a0e9fc84332abf02fca2b6336335f
heguicun <heguicun7878@foxmail.com>
2022-09-27 12:41:44 +0800

    “界面bug细节调整”

--- app/src/main/java/com/dream/bjst/account/ui/AccountFragment.kt
+++ app/src/main/java/com/dream/bjst/account/ui/AccountFragment.kt
@@ -3,12 +3,14 @@ package com.dream.bjst.account.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.blankj.utilcode.util.LogUtils
 import com.dream.bjst.R
 import com.dream.bjst.account.vm.AccountViewModel
 import com.dream.bjst.common.UserManager
 import com.dream.bjst.databinding.FragmentAccountBinding
 import com.dream.bjst.identification.ui.ApproveMainActivity
 import com.dream.bjst.loan.ui.LoanRecordsActivity
+import com.dream.bjst.other.WebViewActivity
 import com.dream.bjst.utils.StatusBarUtils.adjustWindow
 import com.tcl.base.common.ui.BaseFragment
 import com.tcl.base.kt.ktClick
@@ -20,6 +22,7 @@ import com.tcl.base.utils.MmkvUtil
 class AccountFragment : BaseFragment<AccountViewModel, FragmentAccountBinding>() {
 
     override fun initView(savedInstanceState: Bundle?) {
+        viewModel.privacy()
         adjustWindow(requireActivity(), R.color.color_F8FFF0, mBinding.topLay)
         //设置用户姓名
         mBinding.accountName.text=UserManager.getUserName()
@@ -48,7 +51,14 @@ class AccountFragment : BaseFragment<AccountViewModel, FragmentAccountBinding>()
         //隐私界面
 
         mBinding.accountPrivacyPolice.ktClick {
-            ktStartActivity(PrivacyActivity::class)
+            //跳转隐私政策
+            viewModel.privacyResult.value?.`84869D8295978DB59386919199919A80A18698`?.let {
+                ktStartActivity(WebViewActivity::class) {
+                    putExtra("webUrl", it)
+                }
+            }
+
+//            ktStartActivity(PrivacyActivity::class)
         }
 
         //顾客聊天服务
@@ -68,5 +78,19 @@ class AccountFragment : BaseFragment<AccountViewModel, FragmentAccountBinding>()
         }
 
     }
+//
+//    override fun startObserve() {
+//        super.startObserve()
+//        viewModel.privacyResult.observe(this) {
+//            mBinding.webView.loadUrl(it.`849186999D87879D9B9AB18C8498959D9AA18698`)//权限说明地址
+//            var privacyAgreementBreviaryUrl =
+//                it.`84869D8295978DB59386919199919A80B68691829D95868DA18698`//隐私权限（简）地址
+//            var privacyAgreementUrl = it.`84869D8295978DB59386919199919A80A18698`//隐私协议地址
+//            var registerAgreementUrl = it.`8691939D87809186B59386919199919A80A18698`//注册协议地址
+//            LogUtils.dTag("urlTag", "privacyAgreementBreviaryUrl = $privacyAgreementBreviaryUrl")
+//            LogUtils.dTag("urlTag", "privacyAgreementUrl = $privacyAgreementUrl")
+//            LogUtils.dTag("urlTag", "registerAgreementUrl = $registerAgreementUrl")
+//        }
+//    }
 
 }

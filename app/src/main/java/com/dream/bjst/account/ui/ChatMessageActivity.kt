758d1fb07260b5a85b8399173a600e726292c8d1
aliyun0100814462-B18PT <986812333@qq.com>
2022-10-08 19:30:23 +0800

    fghjk

--- app/src/main/java/com/dream/bjst/account/ui/ChatMessageActivity.kt
+++ app/src/main/java/com/dream/bjst/account/ui/ChatMessageActivity.kt
@@ -1,21 +1,23 @@
 package com.dream.bjst.account.ui
 
+import android.annotation.TargetApi
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.webkit.*
-import com.blankj.utilcode.util.GsonUtils
-import com.blankj.utilcode.util.ToastUtils
-import com.didichuxing.doraemonkit.util.LogUtils
 import com.dream.bjst.account.vm.AccountViewModel
+import com.dream.bjst.common.Constant
 import com.dream.bjst.common.UserManager
 import com.dream.bjst.databinding.ActivityChatMessageBinding
 import com.tcl.base.common.ui.BaseActivity
 import com.tcl.base.kt.ktClick
 
+
 class ChatMessageActivity : BaseActivity<AccountViewModel, ActivityChatMessageBinding>() {
-    //    val url = "https://www.baidu.com/"
     var isError: Boolean = false
 
     override fun initView(savedInstanceState: Bundle?) {
@@ -39,13 +41,12 @@ class ChatMessageActivity : BaseActivity<AccountViewModel, ActivityChatMessageBi
     override fun startObserve() {
         super.startObserve()
         viewModel.chatMessageResult.observe(this) {
-            if (it.`978187809B999186A79186829D9791A7839D80979C`){
-                var value="?customerId=${UserManager.getUserNo()}&userName=${UserManager.getUserName()}&userPhone=${UserManager.getUserPhone()}&customerUId=${UserManager.getCustomerUid()}"
+            if (it.`978187809B999186A79186829D9791A7839D80979C`) {
+                var value =
+                    "?customerId=${UserManager.getUserNo()}&userName=${UserManager.getUserName()}&userPhone=${UserManager.getUserPhone()}&customerUId=${UserManager.getCustomerUid()}"
                 var chatUrl = it.`978187809B999186B7958691B79C9580A495809C`
-                mBinding.messageWeb.loadUrl(chatUrl+value)
+                mBinding.messageWeb.loadUrl(chatUrl + value)
             }
-            //ToastUtils.showShort("Close the customer service self-developed system !")
-
         }
     }
 
@@ -96,7 +97,9 @@ class ChatMessageActivity : BaseActivity<AccountViewModel, ActivityChatMessageBi
         //装载url
 //        mBinding.messageWeb.loadUrl(url)
         // Set web view client
-        mBinding.messageWeb.webViewClient = object : WebViewClient() {
+        mBinding.messageWeb.webChromeClient = mWebViewClient
+
+        /*mBinding.messageWeb.webViewClient = object : WebViewClient() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                 // Page loading started
@@ -128,9 +131,76 @@ class ChatMessageActivity : BaseActivity<AccountViewModel, ActivityChatMessageBi
                 }
             }
 
-        }
+        }*/
     }
 
     override fun initDataOnResume() {
+    }
+
+    private var mUploadMessage: ValueCallback<Uri?>? = null
+    private var mUploadCallbackAboveL: ValueCallback<Array<Uri?>>? = null
+    private val FILE_CHOOSER_RESULT_CODE = 200
+
+    private val mWebViewClient: WebChromeClient = object : WebChromeClient() {
+        // For Android >= 5.0
+        override fun onShowFileChooser(
+            webView: WebView?,
+            filePathCallback: ValueCallback<Array<Uri?>>?,
+            fileChooserParams: FileChooserParams?
+        ): Boolean {
+            mUploadCallbackAboveL = filePathCallback
+            goPhotoAlbum()
+            return true
+        }
+    }
+
+    fun goPhotoAlbum() {
+        val intent: Intent
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+        } else {
+            intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        }
+        startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+            if (null == mUploadMessage && null == mUploadCallbackAboveL) return
+            val result = if (data == null || resultCode != RESULT_OK) null else data.data
+            if (mUploadCallbackAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data)
+            } else if (mUploadMessage != null) {
+                mUploadMessage?.onReceiveValue(result)
+                mUploadMessage = null
+            }
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun onActivityResultAboveL(requestCode: Int, resultCode: Int, intent: Intent?) {
+        if (requestCode != FILE_CHOOSER_RESULT_CODE || mUploadCallbackAboveL == null) return
+        var results: Array<Uri?>? = null
+        if (resultCode == RESULT_OK) {
+            if (intent != null) {
+                val dataString = intent.dataString
+                val clipData = intent.clipData
+                if (clipData != null) {
+                    results = arrayOfNulls(clipData.itemCount)
+                    for (i in 0 until clipData.itemCount) {
+                        val item = clipData.getItemAt(i)
+                        results[i] = item.uri
+                    }
+                }
+                if (dataString != null) results = arrayOf(Uri.parse(dataString))
+            }
+        }
+        results?.let {
+            mUploadCallbackAboveL?.onReceiveValue(it)
+            mUploadCallbackAboveL = null
+        }
     }
 }

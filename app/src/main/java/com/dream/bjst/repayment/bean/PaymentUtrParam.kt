cb6189bf218b270b1564b8bc20e06827c244c554
heguicun <heguicun7878@foxmail.com>
2022-10-09 21:59:12 +0800

    “界面上传utr 以及图片bug调整”

--- app/src/main/java/com/dream/bjst/repayment/bean/PaymentUtrParam.kt
+++ app/src/main/java/com/dream/bjst/repayment/bean/PaymentUtrParam.kt
@@ -9,4 +9,5 @@ data class PaymentUtrParam(
     var `86919995869F`: String? = null,//描述
     var `95999B819A80`: String? = null, //贷款数量
     var `969B86869B83BD90`: String? = null,//borrowId
+    var `979B9984869187879190`: Boolean? = null,//图片是否经过加压
 ) : BaseParamBean(), Serializable
